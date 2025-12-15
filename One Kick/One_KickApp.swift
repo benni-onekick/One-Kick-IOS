6faf68718523e753087eed03d06eea42a03383f3
Benni-One-Kick <benjamin.andreas99@hotmail.de>
2025-12-15 12:43:52 +0100

    Initial Commit

---
+++ One Kick/One_KickApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_KickApp.swift
+//  One Kick
+//
+//  Created by Benjamin Diedrich on 15.12.25.
+//
+
+import SwiftUI
+
+@main
+struct One_KickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
